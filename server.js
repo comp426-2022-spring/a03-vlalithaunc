ff70c6312bc9c4c392da1d6e03a9026dd126019a
vlalithaunc <vlalitha@email.unc.edu>
2022-03-02 16:17:22 -0500

    added another endpoint

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const server = app.listen(port, () => {
     console.log('App is running on %PORT%'.replace('%PORT%', port))
 })
 
+app.get('/app', (req, res) => {
+    res.status(200).end('OK')
+    res.type("text/plain")
+})
+
 app.use(function(req, res){
     res.status(404).send("Endpoint does not exist")
     res.type("text/plain")
